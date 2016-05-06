c36fa2cf1eddff497515a241ba28ce453e485040
Joao Antonio Ferreira <joao.parana@gmail.com>
2016-05-06 10:47:03 -0300

    Adicionando código do exemplo

---
+++ ex-0-1/run-app.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+APP_NAME=ss31-ex-0-1
+if [ $1 = 'build' ];
+then
+  mvn clean
+  mvn package && rm -rf tomcat8x/webapps/$APP_NAME*
+  cp target/$APP_NAME-1.0.war tomcat8x/webapps/$APP_NAME.war
+fi
+# 
+rm -rf tomcat8x/logs/*.log 
+rm -rf tomcat8x/logs/*.txt
+cd tomcat8x/bin/
+./run-tomcat.sh
+
